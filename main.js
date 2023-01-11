0b34876de4d765d7b5d5cd765f023caaf8b20636
sasha sims <solely.sashasims@gmail.com>
2023-01-10 19:55:20 -0500

    fixed problem with checked items and added CSS

--- main.js
+++ main.js
@@ -7,9 +7,25 @@ let lastName = travelForm.lastName.value;
 let age = travelForm.age.value;
 let gender = travelForm.gender.value;
 let location = travelForm.location.value;
-let diet = travelForm.diet.value;
 
 
-alert(`First Name: ${firstName} Last Name: ${lastName} Age: ${age} Gender: ${gender} Location: ${location} Diet: ${diet}  `)
+
+const checkedDiet = [];
+for(let i = 0; i < travelForm.diet.length; i++){
+
+    if(travelForm.diet[i].checked){
+        checkedDiet.push(travelForm.diet[i].value)
+    }
+}
+
+alert(`First Name: ${firstName}
+Last Name: ${lastName}
+Age: ${age} Gender: ${gender}
+Location: ${location} 
+Dietary Restriction: ${checkedDiet}  `)
+
+
+
+
 
 })
